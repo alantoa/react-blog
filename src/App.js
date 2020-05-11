3d9206f5f22df3ef1df09e86c2ade5e336a35963
Toa <496807117@qq.com>
2020-05-11 18:42:36 +0800

    react-blog Day1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <Button
+        variant="contained"
+        color="primary">
+        你好，世界
+      </Button>
+    )
+      ;
+  }
+
+}
+
+export default App;
