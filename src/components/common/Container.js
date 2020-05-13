272fea141bc55a6a4624a8930e25f7beedf2773b
Toa <496807117@qq.com>
2020-05-13 18:56:21 +0800

    react-blog Day3

--- src/components/common/Container.js
+++ src/components/common/Container.js
@@ -3,15 +3,16 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 // import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import RouterView from '../../routes/index'
+
 export default class MyContainer extends React.Component {
   render() {
     return (
       <React.Fragment>
-      <CssBaseline />
-      <Container>
-        <RouterView/>
-      </Container>
-    </React.Fragment>
+        <CssBaseline/>
+        <Container>
+          <RouterView/>
+        </Container>
+      </React.Fragment>
     )
   }
 }
