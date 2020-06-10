236ea9173a92960a7830365890641c6fe0f66a0f
Toa <496807117@qq.com>
2020-06-10 15:48:13 +0800

    Day 24

--- src/components/LoadingComponent/index.jsx
+++ src/components/LoadingComponent/index.jsx
@@ -1,14 +1,23 @@
-import React from "react";
-
+import React from 'react'
+import style from "./Loading.module.scss"
+import clsx from "clsx";
 export default function LoadingComponent(isLoading, error) {
   // Handle the loading state
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className={style.background}>
+        <div className={style.loader}>
+          <div className={clsx(style.inner, style.one)} />
+          <div className={clsx(style.inner, style.two)} />
+          <div className={clsx(style.inner, style.three)} />
+        </div>
+      </div>
+    )
   }
   // Handle the error state
   else if (error) {
-    return <div>Sorry, there was a problem loading the page.</div>;
+    return <div>Sorry, there was a problem loading the page.</div>
   } else {
-    return null;
+    return null
   }
 }
