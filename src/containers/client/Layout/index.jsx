3c90c76bece7932622d8b05000a058c47972ec85
Toa <496807117@qq.com>
2020-06-14 01:03:41 +0800

    Day 29

--- src/containers/client/Layout/index.jsx
+++ src/containers/client/Layout/index.jsx
@@ -3,7 +3,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Header from "../Header";
 import Footer from "../Footer";
-import Banner from "@/components/Banner";
+import Banner from "../Banner";
 
 
 
