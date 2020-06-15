cd89387e112c74ef247d86737baa1bda1994c81d
Toa <496807117@qq.com>
2020-06-15 18:32:33 +0800

    Day31

---
+++ src/views/AdminAddArticle/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default class ArticleManage extends React.Component {
+
+  render() {
+
+    return (
+      <div className="pageView">
+        
+      </div>
+    )
+  }
+}
