e8ca972a9e7bd6ae1bde4a6e76bbc6e38cfd371a
Toa <496807117@qq.com>
2020-07-01 23:32:19 +0800

    Day43

--- src/views/article/TimeLine/index.jsx
+++ src/views/article/TimeLine/index.jsx
@@ -1,72 +1,56 @@
-import React from 'react';
-import Timeline from '@material-ui/lab/Timeline';
-import TimelineItem from '@material-ui/lab/TimelineItem';
-import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
-import TimelineConnector from '@material-ui/lab/TimelineConnector';
-import TimelineContent from '@material-ui/lab/TimelineContent';
-import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
-import TimelineDot from '@material-ui/lab/TimelineDot';
-import FastfoodIcon from '@material-ui/icons/Fastfood';
-import LaptopMacIcon from '@material-ui/icons/LaptopMac';
-import HotelIcon from '@material-ui/icons/Hotel';
-import RepeatIcon from '@material-ui/icons/Repeat';
-import Typography from '@material-ui/core/Typography';
-import CardList from '../CardList'
+import React from "react";
+import Timeline from "@material-ui/lab/Timeline";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
+import TimelineConnector from "@material-ui/lab/TimelineConnector";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import Typography from "@material-ui/core/Typography";
+import CardList from "../CardList";
 
 export default function CustomizedTimeline() {
   return (
     <Timeline align="alternate">
       <TimelineItem>
-        <TimelineOppositeContent>
+        <TimelineSeparator>
           <Typography variant="body2" color="textSecondary">
             9-30
           </Typography>
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot>
-            <FastfoodIcon />
-          </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>
-          <CardList/>
+          <CardList />
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
-        <TimelineOppositeContent>
+        <TimelineSeparator>
           <Typography variant="body2" color="textSecondary">
-            9-02
+            9-30
           </Typography>
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot color="primary">
-            <LaptopMacIcon />
-          </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>
-            <CardList/>
+          <CardList />
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
         <TimelineSeparator>
-          <TimelineDot color="primary" variant="outlined">
-            <HotelIcon />
-          </TimelineDot>
-          <TimelineConnector/>
+          <Typography variant="body2" color="textSecondary">
+            9-30
+          </Typography>
+          <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>
-            <CardList/>
+          <CardList />
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
         <TimelineSeparator>
-          <TimelineDot color="secondary">
-            <RepeatIcon />
-          </TimelineDot>
+          <Typography variant="body2" color="textSecondary">
+            9-30
+          </Typography>
         </TimelineSeparator>
         <TimelineContent>
-            <CardList/>
+          <CardList />
         </TimelineContent>
       </TimelineItem>
     </Timeline>
